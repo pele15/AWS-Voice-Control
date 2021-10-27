34b74cff464b1fdc8ae38b171441f6b62594cd39
Praneeth Eddu <pele15@carbonorigins.com>
2021-10-26 22:50:42 -0500

    twin-ignitions

--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 import numpy as np
 from PIL import Image
+from matplotlib import pyplot as plt
 
-def resizeImage(img, panels=6):
+def restructureImage(img, outputFileName, panels=6):
     if (panels == 6):
         img = Image.open(img)
+        img = np.array(img)
+        img_top = img[:int(img.shape[0] / 3) , :img.shape[1], :]
+        img_middle = img[int(img.shape[0] / 3) : int(img.shape[0] / 3) * 2, :img.shape[1], :]
+        img_middle = np.flip(img_middle, axis=1)
+        img_bottom = img[int(img.shape[0] / 3) * 2 : , :img.shape[1], :]
+        
+        stitched_img = np.empty((img_top.shape[0], img_top.shape[1] * 3, img_top.shape[2]))
+        stitched_img = np.hstack((img_top, img_middle, img_bottom))
+        stitched_img = Image.fromarray(stitched_img)
+        stitched_img.save(outputFileName)
+
+
+
+restructureImage("test/twin-ignitions.png", "twin-ignitions.jpg")

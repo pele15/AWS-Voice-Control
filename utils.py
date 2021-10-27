0664e8e7f04957fa27a635649edadf1f48e6c1b6
Praneeth Eddu <pele15@carbonorigins.com>
2021-10-26 23:04:06 -0500

    twins

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def restructureImage(img, outputFileName, panels=6):
         img_top = img[:int(img.shape[0] / 3) , :img.shape[1], :]
         img_middle = img[int(img.shape[0] / 3) : int(img.shape[0] / 3) * 2, :img.shape[1], :]
         img_middle = np.flip(img_middle, axis=0)
+        img_middle = np.flip(img_middle, axis=1)
         img_bottom = img[int(img.shape[0] / 3) * 2 : , :img.shape[1], :]
         
         stitched_img = np.empty((img_top.shape[0], img_top.shape[1] * 3, img_top.shape[2]))

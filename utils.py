4d6240c68e088be2dc0884e1519f8b1027f5530f
Praneeth Eddu <pele15@carbonorigins.com>
2021-10-27 10:20:27 -0500

    utils

--- utils.py
+++ utils.py
@@ -4,7 +4,11 @@ from matplotlib import pyplot as plt
 
 def restructureImage(img, outputFileName, panels=6):
     img = Image.open(img)
+    img = img.convert('RGB')
     img = np.array(img)
+    
+    print(img.shape)
+    img = resizeImage(img, panels)
     if (panels == 6):
 
         img_top = img[:int(img.shape[0] / 3) , :img.shape[1], :]
@@ -12,13 +16,15 @@ def restructureImage(img, outputFileName, panels=6):
         img_middle = np.flip(img_middle, axis=0)
         img_middle = np.flip(img_middle, axis=1)
         img_bottom = img[int(img.shape[0] / 3) * 2 : , :img.shape[1], :]
-        
+        print(img_top.shape)
+        print(img_middle.shape)
+        print(img_bottom.shape)
         stitched_img = np.empty((img_top.shape[0], img_top.shape[1] * 3, img_top.shape[2]))
         stitched_img = np.hstack((img_top, img_middle, img_bottom))
 
     
     if (panels == 4):
-        img_top = img[:int(img.shape[0] / 2) , :img.shape[1], :]
+        img_top = img[:int(img.shape[0] / 2) , :img.shape[1], :img.shape[2]]
         img_top = np.flip(img_top, axis=0)
         img_top = np.flip(img_top, axis=1)
         img_bottom = img[int(img.shape[0] / 2) :, :img.shape[1], :]
@@ -31,6 +37,10 @@ def restructureImage(img, outputFileName, panels=6):
     stitched_img = Image.fromarray(stitched_img)
     stitched_img.save(outputFileName)
 
-
+def resizeImage(img, panels):
+    if (img.shape[0] % (panels / 2) != 0):
+        img = img[int(img.shape[0] % (panels / 2)) : , :, :]    
+        print(img.shape)
+    return img
 
 restructureImage("test/twin-ignitions.png", "twin-ignitions-4.jpg", panels=4)
